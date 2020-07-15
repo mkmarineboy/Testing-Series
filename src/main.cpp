0e7f28aa2a3024b2d2dc145162b2e699755fadcd
Muhammet <iletisim@muhammetkucuk.com>
2020-07-15 15:43:04 +0300

    Multiple Targets Sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "I'm from Main Target";
 
     return a.exec();
 }
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "I'm from Test Target";
+
+    return a.exec();
+}
